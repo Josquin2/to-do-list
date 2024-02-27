321e67e6f8a15abd1857f49d5fbb6b0fb0f070b7
Josquin2 <Josquin2@mail.ru>
2024-02-27 11:58:38 +0300

    added few styles and an empty error

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,23 @@ function addInput(){
     let a = document.getElementById('input').value;
     let li = document.createElement('li');
     li.innerHTML = `<div id="alo" class = "elem">${a} <img class="bin" src="/images/bin.jpg" alt="bin" onclick="deleteElem(this)"></div>`;
-    listItems.appendChild(li);
+    if (a != ''){
+        listItems.appendChild(li);
+    document.getElementById('input').value = '';
+    }
+    else{
+        Errors('empty')
+    }
+}
+
+function Errors(code){
+    if (code == 'empty'){
+        document.getElementById('alerts').innerHTML = 'input is empty!'
+        setTimeout(() => document.getElementById('alerts').innerHTML = '', 2000)
+    }
+    else if (code == 'exists'){
+        // toDO: if input exists in array inputList => alert 'already exists!'
+    }
 }
 // its not working correctly
 // TODO: write an array inputList with all inputs
